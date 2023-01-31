d808bd573cb97b4561d9f72215d14ff04395b75c
Yucheng Lu <yl5163@columbia.edu>
2023-01-31 14:23:22 -0500

    branch merging test

--- junk.sh
+++ junk.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 ####################
-# two functions needed to reuse
+# two functions needed to repeat below
+
 Usage (){
     echo "Usage: junk.sh [-hlp] [list of files]"
     echo "  -h: Display help."
     echo "  -l: List junked files."
     echo "  -p: Purge all files."
     echo "  [list of files] with no other arguments to junk those files."
-    exit 1
+    # no needs to exit
 }
 
 err (){
     echo "Error: $1"
     Usage
-    exit 1
+    exit 1 # exit 1 means error
 }
 
 #check directory exists or create one
@@ -32,8 +33,8 @@ while getopts "hlp" option; do
     case $option in
       h) Usage # Display Help
          exit 0;;
-      l) list=true;; # List junked files.     
-      p) purge=true;; # Purge all files.
+      l) list=true;; # List junked files     
+      p) purge=true;; # Purge all files
      \?) err "Error: Unknown option -$OPTARG."
     esac
 done
@@ -43,19 +44,19 @@ if [ "$list" = true ] && [ "$purge" = true ]; then
     err "Too many options enabled."
 fi
 
-# to get the arguement after hlp
+# to get the arguement after hlp, moving to the next command
 shift $((OPTIND -1))
 
 # Check the last situation, when files are specified along with the flag
 if [ "$list" = true ] || [ "$purge" = true ]; then ## when l or p exists
-    if [ $# -ne 0 ]; then # when the files exist
+    if [ $# -ne 0 ]; then # while the file exists as well
         err "Too many options enabled."
     fi
 fi
 
 # List junked files if -l flag is specified
 if [ "$list" = true ]; then
-    ls -lAF ~/.junk
+    ls -lAF ~/.junk # Combine these arguments for a long format list of all files with types
     exit 0 # success
 fi
 
