234a2af3f39c8ea117bf3623d457ca8b523f116b
Yucheng Lu <yl5163@columbia.edu>
2023-01-31 00:43:59 -0500

    corrected typos

--- junk.sh
+++ junk.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 ####################
-function Usage (){
+# two functions needed to reuse
+Usage (){
     echo "Usage: junk.sh [-hlp] [list of files]"
     echo "  -h: Display help."
     echo "  -l: List junked files."
     echo "  -p: Purge all files."
     echo "  [list of files] with no other arguments to junk those files."
+    exit 1
+}
+
+err (){
+    echo "Error: $1"
+    Usage
     exit 1
 }
 
@@ -16,29 +23,24 @@ fi
 
 # Check if there is no arguments
 if [ $# -eq 0 ]; then
-  Usage
-  exit 0
+    Usage
+    exit 0
 fi
 
+# read the input command
 while getopts "hlp" option; do
     case $option in
       h) Usage # Display Help
          exit 0;;
-      l) list=true # List junked files.
-         exit 0;;      
-      p) purge=true # Purge all files.
-         exit 0;;      
-     \?) echo "Error: Unknown option -$OPTARG."
-         Usage
-         exit 1;;
+      l) list=true;; # List junked files.     
+      p) purge=true;; # Purge all files.
+     \?) err "Error: Unknown option -$OPTARG."
     esac
 done
 
 # Check if more than one flag is specified
 if [ "$list" = true ] && [ "$purge" = true ]; then      
-    echo "Too many options enabled."
-    Usage
-    exit 1;;
+    err "Too many options enabled."
 fi
 
 # to get the arguement after hlp
@@ -47,8 +49,6 @@ shift $((OPTIND -1))
 # Check the last situation, when files are specified along with the flag
 if [ "$list" = true ] || [ "$purge" = true ]; then ## when l or p exists
     if [ $# -ne 0 ]; then # when the files exist
-        echo "Too many options enabled."
-        Usage
-        exit 1;;
+        err "Too many options enabled."
     fi
 fi
